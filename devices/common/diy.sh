489ad9a7883f47503fbcf84b854b9bd3cd76cd49
leesuncom <70786826+leesuncom@users.noreply.github.com>
2023-12-10 13:14:46 +0800

    Update diy.sh

--- devices/common/diy.sh
+++ devices/common/diy.sh
@@ -60,11 +60,13 @@ curl -sfL https://raw.githubusercontent.com/coolsnowwolf/lede/master/target/linu
 sed -i "s/CONFIG_WERROR=y/CONFIG_WERROR=n/" target/linux/generic/config-5.15
 
 curl -sfL https://github.com/sbwml/luci-app-mosdns/raw/v5/luci-app-mosdns/root/etc/hotplug.d/iface/99-mosdns -o feeds/kiddin9/luci-app-mosdns/root/etc/hotplug.d/iface/99-mosdns
-# curl -sfL https://github.com/leesuncom/smartdns/raw/23.05/Makefile -o feeds/kiddin9/smartdns/Makefile
-# curl -sfL https://github.com/leesuncom/smartdns/raw/23.05/conf/* -o feeds/kiddin9/smartdns/conf/
-# rm -rf package/feeds/kiddin9/smartdns
 rm -rf feeds/kiddin9/smartdns
 git clone -b 23.05 https://github.com/leesuncom/smartdns.git feeds/kiddin9/smartdns
+sed -i "154i uci set network.lan.gateway=192.168.1.1" feeds/kiddin9/my-default-settings/files/etc/uci-defaults/99-default-settings
+sed -i "155i uci set network.lan.dns=127.0.0.1" feeds/kiddin9/my-default-settings/files/etc/uci-defaults/99-default-settings
+sed -i 's/OpenWrt/NeoBird/g' feeds/kiddin9/my-default-settings/files/etc/uci-defaults/99-default-settings
+sed -i 's/^IMG_PREFIX\:\=.*/IMG_PREFIX:=$(VERSION_DIST_SANITIZED)-$(date +"%Y.%m.%d-%H%M")-$(IMG_PREFIX_VERNUM)$(IMG_PREFIX_VERCODE)$(IMG_PREFIX_EXTRA)$(BOARD)$(if $(SUBTARGET),-$(SUBTARGET))/g' include/image.mk
+
 
 grep -q "23.05" include/version.mk && [ -d package/kernel/mt76 ] && {
 mkdir package/kernel/mt76/patches
